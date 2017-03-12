bf8845dda84ec48c1880d8843e772651c74efe91
Andreas Spielhofer <andreas.spielhofer@mail.mcgill.ca>
2017-03-12 15:35:32 -0400

    Update serialscan.py

--- drivers/serialscan.py
+++ drivers/serialscan.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python
 """\
 Scan for serial ports.
-
-Part of pySerial (http://pyserial.sf.net)
-(C) 2002-2003 <cliechti@gmx.net>
-
 The scan function of this module tries to open each port number
 from 0 to 255 and it builds a list of those ports where this was
 successful.
